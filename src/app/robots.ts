19ffbe640299edf68adc7e2dd96a6e0e9c4ba1f1
vizoholic <vizoholic@gmail.com>
2026-03-20 20:27:41 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,11 @@
+// src/app/robots.ts
 import { MetadataRoute } from 'next'
-
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: '/api/' },
-    sitemap: 'https://productrating.in/sitemap.xml',
+    rules: [
+      { userAgent: '*', allow: '/', disallow: ['/api/', '/search?q='] },
+    ],
+    sitemap: 'https://www.productrating.in/sitemap.xml',
+    host: 'https://www.productrating.in',
   }
 }
